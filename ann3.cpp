4b26dcf41056b74563053259b8bb9198198395fe
Brandon Addison <addbra001@myuct.ac.za>
2018-09-20 00:30:15 +0200

    ann fully functional, just writing to file needed for completion

--- ann3.cpp
+++ ann3.cpp
@@ -13,6 +13,11 @@ using namespace std;
 
 vector < pair<float,float> > node;
 
+float sigmoid (float net) {
+  float o = 1/(1 + exp(-net) );
+  return o;
+}
+
 float node_output(vector< pair<float,float> > inputs) {
   float output = 0;
   for (int i = 0; i < inputs.size(); i++) {
@@ -27,10 +32,6 @@ float node_output(vector< pair<float,float> > inputs) {
 
 }
 
-float sigmoid (float net) {
-  float o = 1/(1 + exp(-net) );
-  return o;
-}
 
 float output_node_error(float o, float t) {
   float e = o*(1-o)*(t-o);
@@ -64,11 +65,19 @@ int main () {
   float w12 = 0;
   float w22 = 1;
 
+  // target outputs
+  float t1 = 1;
+  float t2 = 0;
+
+  // learning rate
+  float n = 0.1f;
+
+
   pair <float, float> x1n1(x1,v11);
   pair <float, float> x1n2(x1,v21);
 
   pair <float, float> x2n1(x2,v12);
-  pair <float, float> x2n2(x1,v22);
+  pair <float, float> x2n2(x2,v22);
 
   node.push_back(x1n1);
   node.push_back(x2n1);
@@ -100,7 +109,41 @@ int main () {
   cout << y2 << endl;
   node.clear();
 
+  float y1_error = output_node_error(y1,t1);
+  float y2_error = output_node_error(y2,t2);
+  cout << y1_error << endl << y2_error << endl;
 
+  pair <float,float> w11y1e(w11,y1_error);
+  pair <float,float> w21y2e(w21,y2_error);
+  pair <float,float> w12y1e(w12,y1_error);
+  pair <float,float> w22y2e(w22,y2_error);
+  node.push_back(w11y1e);
+  node.push_back(w21y2e);
+  //node.push_back(w12y1e);
+  //node.push_back(w22y2e);
+  float n1_error = hidden_node_error(n1, node);
+  node.clear();
+
+  node.push_back(w12y1e);
+  node.push_back(w22y2e);
+  float n2_error = hidden_node_error(n2, node);
+  node.clear();
+
+  w11+= n*y1_error*n1;
+  w21+= n*y2_error*n1;
+  w12+= n*y1_error*n2;
+  w22+= n*y2_error*n2;
+  cout << w11 << endl << w21 << endl << w12 << endl << w22 << endl;
+
+  cout << n1_error << endl << n2_error << endl;
+
+  v11+= n*n1_error*x1;
+  v21+= n*n2_error*x1;
+  v12+= n*n1_error*x2;
+  v22+= n*n2_error*x2;
+
+
+  //float n1_error = hidden_node_error()
 
 
   return 0;
